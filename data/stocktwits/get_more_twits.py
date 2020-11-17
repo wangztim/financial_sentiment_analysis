8831cfec91abe39793d0f5314243e260bfa2d2d4
Tim Wang <timzwang@outlook.com>
2020-11-17 12:35:45 -0500

    temp

--- data/stocktwits/get_more_twits.py
+++ data/stocktwits/get_more_twits.py
@@ -1,4 +1,3 @@
-import requests
 import time
 import csv
 import os
@@ -131,69 +130,12 @@ async def main():
 
             print('restarting VPN process.')
             call(["killall", vpn])
-            call(["./vpnutil", "stop", vpn], cwd=pathlib.Path.home())
             time.sleep(2)
             print("starting " + vpn)
             call(["open", "-a", vpn])
             print("waiting some time to let connection get established")
-            time.sleep(12)
+            time.sleep(16)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-# def getExhaustedStocks():
-#     exh_path = os.path.dirname(
-#         os.path.abspath(__file__)) + "/exhausted_stocks.txt"
-#     with open(exh_path, "r") as f:
-#         return f.read().splitlines()
-
-
-# def addExhaustedStock(name):
-#     exh_path = os.path.dirname(
-#         os.path.abspath(__file__)) + "/exhausted_stocks.txt"
-#     with open(exh_path, "a") as f:
-#         return f.write(name + "\n")
-
-
-# while True:
-#     # exhausted_stocks = getExhaustedStocks()
-#     # good_tickers = [t for t in sp_500_tickers if t not in exhausted_stocks]
-#     seq = list(range(0, len(sp_500_tickers)))
-#     random.shuffle(seq)
-
-#     vpns = ["ProtonVPN", "Hotspot Shield"]
-#     vpn_idx = 0
-
-#     for ticker_idx in seq:
-#         ticker = sp_500_tickers[ticker_idx]
-#         try:
-#             print(ticker)
-#             fetchAndWriteTwits(ticker)  # Is this legal?
-#         except requests.exceptions.ConnectionError as er:
-# print('restarting VPN process.')
-# for vpn in vpns:
-#     call(["killall", vpn], stdout=DEVNULL,
-#          stderr=DEVNULL)
-# vpn_to_use = vpns[vpn_idx]
-# print("starting " + vpn_to_use)
-# call(["open", "-a", vpn_to_use])
-# time.sleep(10)
-# while True:
-#     print("waiting until connection is established")
-#     try:
-#         conn = requests.head("https://www.google.com", timeout=5)
-#         if conn.status_code == 200:
-#             break
-#     except:
-#         pass
-# vpn_idx += 1
-# if vpn_idx >= len(vpns):
-#     vpn_idx = 0
-#         except requests.exceptions.HTTPError as er:
-#             print('unable to fetch information because of error: ' + str(er))
-#             continue
-#         except RuntimeError as er:
-#             print("Skipping because " + str(er))
-#             addExhaustedStock(ticker)
-#             continue
