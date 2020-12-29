80a42c9f7120158c3992a5c02fac307159004038
Tim Wang <timzwang@outlook.com>
2020-12-29 14:23:10 -0500

    implemented reddit fetch with praw

--- classes/fetchers.py
+++ classes/fetchers.py
@@ -5,9 +5,7 @@ from dateutil import parser
 import requests
 import os
 import aiohttp
-import asyncio
-from typing import Tuple
-
+import asyncpraw
 from enum import Enum
 
 
@@ -48,7 +46,9 @@ class StocktwitsFetcher(MessageFetcher):
 
         if params is None:
             params = self.__initParams(ticker)
-        async with session.get(endpoint, params=params, headers=headers,
+        async with session.get(endpoint,
+                               params=params,
+                               headers=headers,
                                timeout=8) as res:
             status_code = res.status
             if status_code == 200:
@@ -56,7 +56,7 @@ class StocktwitsFetcher(MessageFetcher):
                 messages = json["messages"]
                 return [self.processFetched(m, ticker) for m in messages]
             elif (status_code == 429 or status_code == 403):
-                raise requests.exceptions.ConnectionError("Rate limit exhausted.")
+                raise requests.exceptions.ConnectionError("Rate limited.")
             else:
                 raise RuntimeError("API call unsuccessful. Code: " +
                                    str(res.status_code))
@@ -123,7 +123,8 @@ class StocktwitsFetcher(MessageFetcher):
                 markers["newest"]['id'] = twit['id']
                 markers["newest"]['datetime'] = created_date_time
 
-        message = Message(twit['id'], body, user, created_date_time, "StockTwits",
+        message = Message(twit['id'], body, user,
+                          created_date_time, "StockTwits",
                           Sentiment(sentiment_val), likes, replies)
 
         return message
@@ -165,7 +166,7 @@ class TwitterFetcher(MessageFetcher):
                 ]
                 return filtered_data
             else:
-                raise RuntimeError("API call unsuccessful. Code: " + str(status_code))
+                raise RuntimeError("Failure. Code: " + str(status_code))
 
     def processFetched(self, tweet: {}) -> Message:
         likes = tweet.get('public_metrics', {}).get('like_count')
@@ -178,78 +179,52 @@ class TwitterFetcher(MessageFetcher):
 
 
 class RedditFetcher(MessageFetcher):
-    subreddits = ["r/stocks", "r/wallstreetbets"]
-    endpoint = "http://www.reddit.com/"
+    subreddits = ["stocks", "wallstreetbets", "investing"]
 
-    # Authentication is not necessary!
-    def __init__(self, auth=False):
-        if auth:
-            client_id = os.environ['REDDIT_CLIENT_ID']
-            client_secret = os.environ['REDDIT_CLIENT_SECRET']
-            username = os.environ['REDDIT_USERNAME']
-            password = os.environ['REDDIT_PASSWORD']
+    def __init__(self):
+        client_id = os.environ.get('REDDIT_CLIENT_ID')
+        client_secret = os.environ.get('REDDIT_CLIENT_SECRET')
+        username = os.environ.get('REDDIT_USERNAME')
+        password = os.environ.get('REDDIT_PASSWORD')
 
-            client_auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-            post_data = {
-                "grant_type": "password",
-                "username": username,
-                "password": password
-            }
-            headers = {"User-Agent": "Thunderstorm/1.0 (Linux)"}
-            response = requests.post("https://www.reddit.com/api/v1/access_token",
-                                     auth=client_auth,
-                                     data=post_data,
-                                     headers=headers)
-            if response.status_code == 200:
-                self.access_token = response.json()["access_token"]
-                self.token_type = response.json()["token_type"]
-            else:
-                raise RuntimeError("Unable to authenticate with reddit.")
+        if None in (client_id, client_secret, username, password):
+            raise RuntimeError("Missing authentication info!")
+
+        self.reddit = asyncpraw.Reddit(user_agent="Thunderstorm/1.0 (Linux)",
+                                       client_id=client_id,
+                                       client_secret=client_secret,
+                                       username=username,
+                                       password=password)
 
     async def fetch(self,
                     ticker,
                     session: aiohttp.ClientSession,
                     params=None,
                     headers=None) -> [Message]:
+        subreddit = await self.reddit.subreddit("+".join(self.subreddits))
+        messages = []
+        async for submission in subreddit.search(ticker,
+                                                 sort="relevance",
+                                                 time_filter="day",
+                                                 limit=100):
+            messages.append(self.processFetched(submission))
+            # TODO Add Functionality to parse comment tree
+        return messages
 
-        default_headers = {
-            # "Authorization": self.token_type + " " + self.access_token
-        }
+    def processFetched(self, post) -> Message:
+        if isinstance(post, asyncpraw.models.Submission):
+            return self._processSubmissions(post)
+        elif isinstance(post, asyncpraw.models.Comment):
+            return self._processComments(post)
+        else:
+            return None
 
-        default_params = {
-            "q": '{}'.format(ticker),
-            "limit": 5,
-            "t": 'day',
-            "sort": "new",
-            "restrict_sr": "true",
-        }
+    def _processSubmissions(self, s: asyncpraw.models.Submission) -> Message:
+        full_text = s.title + s.selftext
+        author_id = s.author.name
+        created = datetime.fromtimestamp(s.created_utc)
+        return Message(s.id, full_text, author_id, created, "Reddit",
+                       Sentiment(-69), s.score, s.num_comments)
 
-        async def get_fn(sr):
-            async with session.get(self.endpoint + sr + "/search.json",
-                                   params=default_params,
-                                   headers=default_headers,
-                                   timeout=8) as res:
-                status_code = res.status
-                if status_code == 200:
-                    j = await res.json()
-                    return [
-                        self.processFetched(child["data"])
-                        for child in j["data"]["children"]
-                    ]
-                else:
-                    raise RuntimeError("API call unsuccessful. Code: " +
-                                       str(status_code))
-                    return res
-
-        operations = [get_fn(sr) for sr in self.subreddits]
-        status: Tuple[int] = await asyncio.gather(*operations, return_exceptions=True)
-        return [
-            item for sublist in status for item in sublist if sublist is not Exception
-        ]
-
-    def processFetched(self, post: {}) -> Message:
-        full_text = post.get("title", "") + ": " + post.get("selftext", "")
-        return Message(post["name"], full_text, post["author_fullname"],
-                       datetime.fromtimestamp(post["created_utc"]), "Reddit",
-                       Sentiment(-69), post["ups"] - post["downs"],
-                       post["num_comments"])
+    def _processComments(self, comment: asyncpraw.models.Comment) -> Message:
+        raise NotImplementedError()
