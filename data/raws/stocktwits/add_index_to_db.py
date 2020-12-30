7d6bec7d6e7a17554dec5e6ed004fd54ca45cd45
Tim Wang <timzwang@outlook.com>
2020-12-30 17:32:07 -0500

    added better index chcek script

--- data/raws/stocktwits/add_index_to_db.py
+++ data/raws/stocktwits/add_index_to_db.py
@@ -11,15 +11,18 @@ all_files = [
     for file in glob(os.path.join(path, EXT))
 ]
 
+count = 0
 for path in all_files:
     conn = sql.connect(path)
     curs = conn.cursor()
     command = ("CREATE INDEX idx1 ON messages (created_at, sentiment)")
     curs.execute(command)
-    check_command = ("PRAGMA INDEX_LIST('messages')")
+    check_command = ("PRAGMA INDEX_LIST(messages)")
     out = curs.execute(check_command).fetchall()
     for row in out:
         if "idx1" in row:
-            print("good")
-        else:
-            print("bad")
+            count += 1
+            break
+
+if count == len(all_files):
+    print("all good chief!")
