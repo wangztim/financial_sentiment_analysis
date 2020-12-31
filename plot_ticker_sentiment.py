9c6824e48bcbce87c91dde9c9f48e7d922779332
Tim Wang <timzwang@outlook.com>
2020-12-31 12:19:29 -0500

    I'm so dissapointed in myself what the heck

--- plot_ticker_sentiment.py
+++ plot_ticker_sentiment.py
@@ -7,19 +7,22 @@ def plotTickerSentiment(ticker):
     db_path = f'data/raws/stocktwits/tickers/{ticker}/twits.db'
     print(db_path)
     conn = sql.connect(db_path)
-    cmd = """SELECT AVG(sentiment), DATE(created_at), COUNT(*)
+    cmd = """SELECT AVG(sentiment), strftime('%m/%d %H','created_at'), COUNT(*)
                       FROM messages
                       WHERE sentiment != -69
-                      GROUP BY DATE(created_at)"""
+                      GROUP BY strftime('%Y/%m/%d %H','created_at')"""
     df = pd.read_sql(cmd, conn, parse_dates=["created_at"])
+    print(df)
     df = df.rename(
-        mapper={
+        columns={
             "AVG(sentiment)": "sentiment",
-            "DATE(created_at)": "created_at",
+            "strftime('%m/%d %H','created_at')": "created_at",
             "COUNT(*)": "count"
         })
 
-    px.line(df, x="created_at", y="sentiment")
+    fig = px.line(df, x="created_at", y="sentiment")
+    fig.update_traces(mode="markers+lines")
+    fig.show()
 
 
 plotTickerSentiment("AAPL")
