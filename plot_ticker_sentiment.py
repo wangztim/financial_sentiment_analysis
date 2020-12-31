5ab7cce277f62e69774f130f8335e552eec7ffbd
Tim Wang <timzwang@outlook.com>
2020-12-31 11:06:33 -0500

    Implemented graphing functionality

--- plot_ticker_sentiment.py
+++ plot_ticker_sentiment.py
@@ -1,19 +1,19 @@
 import sqlite3 as sql
-import numpy as np
-import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def plotTickerSentiment(ticker):
     db_path = f'data/raws/stocktwits/tickers/{ticker}/twits.db'
     print(db_path)
     conn = sql.connect(db_path)
-    cursor = conn.cursor()
-    cmd = cursor.execute("""SELECT AVG(sentiment), DATE(created_at), COUNT(*)
+    cmd = """SELECT AVG(sentiment), DATE(created_at), COUNT(*)
                       FROM messages
                       WHERE sentiment != -69
-                      GROUP BY DATE(created_at)""")
-    for res in cmd.fetchall():
-        print(res)
+                      GROUP BY DATE(created_at)"""
+    df = pd.read_sql(cmd, conn, parse_dates=["created_at"])
+    df.plot(x="DATE(created_at)", y="AVG(sentiment)")
+    plt.show()
 
 
 plotTickerSentiment("AAPL")
