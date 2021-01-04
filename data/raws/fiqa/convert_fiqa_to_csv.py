afff2d1417af3cc16f4edef76ec48933292ed1e0
Tim Wang <timzwang@outlook.com>
2021-01-04 10:46:44 -0500

    updated data sources

--- data/raws/fiqa/convert_fiqa_to_csv.py
+++ data/raws/fiqa/convert_fiqa_to_csv.py
@@ -2,15 +2,20 @@ import csv
 import json
 import os
 import uuid
+
+from classes.message import Sentiment, Message, to_dict
 from datetime import date
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
+row_headers = [
+    'id', 'body', 'author', 'created_at', 'source', 'sentiment', 'likes',
+    'replies'
+]
 
 with open(curr_dir + '/posts.json',
           'r') as headlines, open(curr_dir + '/posts.csv',
                                   'a+') as headlines_csv:
     data = json.load(headlines)
-    row_headers = ['id', 'body', 'created_at', 'sentiment']
     writer = csv.DictWriter(headlines_csv,
                             delimiter=',',
                             lineterminator='\n',
@@ -26,17 +31,15 @@ with open(curr_dir + '/posts.json',
             total_sentiment += float(sentiment_score)
 
         avg_sentiment = total_sentiment / len(headline['info'])
-        breaking_point = 0.11
+        breaking_point = 0.05
         if (abs(avg_sentiment) < breaking_point):
-            avg_sentiment = 0
+            avg_sentiment = Sentiment.UNCERTAIN
         elif avg_sentiment >= breaking_point:
-            avg_sentiment = 1
+            avg_sentiment = Sentiment.BULLISH
         else:
-            avg_sentiment = -1
-        row = {
-            'id': uuid.uuid4(),
-            'sentiment': avg_sentiment,
-            'body': headline['sentence'],
-            'created_at': today,
-        }
-        writer.writerow(row)
+            avg_sentiment = Sentiment.BEARISH
+
+        message = Message(uuid.uuid4(), headline['sentence'], uuid.uuid4(),
+                          today, "FIQA", avg_sentiment)
+
+        writer.writerow(to_dict(message))
